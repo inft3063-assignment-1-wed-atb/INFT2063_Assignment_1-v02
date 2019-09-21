fae62f7022fb779c7320ceca2fe04a8e33da62ef
Tin Nguyen <ngutt094@mymail.unisa.edu.au>
2019-09-21 13:18:03 +0930

    [#7] Add transaction confirmation dialog within a while loop
    A confirmation dialog was added that asks users if they would like
    to procceed to transaction. Selecting 'n' prematurely ends the
    program. 'Y' proceeds with the transaction. This confirmation
    continously loops after each transaction until users select 'n'.

--- source/CashRegister.java
+++ source/CashRegister.java
@@ -1,38 +1,56 @@
 import java.util.Scanner;
 
-public class CashRegister
-{
-   public static void main(String[] args)
-   {
-      String s, c;
-      double balance;
-	String dollar_symbol = "$";
+public class CashRegister {
+	public static void main(String[] args) {
+		String s, c;
+		String proceed;
+		double balance;
+		String dollar_symbol = "$";
+		boolean endProgram = false;
 
-      Scanner in = new Scanner(System.in);
+		Scanner in = new Scanner(System.in);
 
-      System.out.print("Please enter cash register's float:");
-      s = in.nextLine();
-      balance = Double.parseDouble(s);
+		System.out.print("Please enter cash register's float:");
+		s = in.nextLine();
+		balance = Double.parseDouble(s);
 
-      System.out.print("Please enter the item's name:");
-      s = in.nextLine();
+		while (!endProgram) {
+			
+			System.out.println("Would you like to proceed with the transaction? (y/n)");
+			proceed = in.nextLine();
 
-      System.out.print("Please enter the item's cost:");
-      c = in.nextLine();
+			if (proceed.equalsIgnoreCase("y")) {
 
-      Transaction trans = new Transaction(s, Double.parseDouble(c));
+				System.out.print("Please enter the item's name:");
+				s = in.nextLine();
 
-      System.out.print("Please enter the cash amount tendered:");
-      s = in.nextLine();
-      c = Double.toString(Double.parseDouble(s) - trans.getCost());
+				System.out.print("Please enter the item's cost:");
+				c = in.nextLine();
 
-      System.out.println("Amount of change required = " + dollar_symbol + c);
+				Transaction trans = new Transaction(s, Double.parseDouble(c));
 
-      c = Double.toString(balance + trans.getCost());
+				System.out.print("Please enter the cash amount tendered:");
+				s = in.nextLine();
+				c = Double.toString(Double.parseDouble(s) - trans.getCost());
 
-      System.out.println("Balance of the Cash Register:" + dollar_symbol + c);
+				System.out.println("Amount of change required = " + dollar_symbol + c);
 
+				c = Double.toString(balance + trans.getCost());
 
+				System.out.println("Balance of the Cash Register:" + dollar_symbol + c);
 
-   }
+			}
+
+			else if (proceed.equalsIgnoreCase("n")) {
+				endProgram = true;
+			}
+
+			else {
+				System.out.println("Invalid input. Please try again");
+			}
+
+		}
+
+	}
 }
+
